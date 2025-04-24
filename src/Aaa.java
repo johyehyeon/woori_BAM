c09051a46d10bd5d27006b0b664acd5e446d3699
johyehyeon <johyehyeon92@gmail.com>
2025-04-24 17:44:14 +0900

    article detail 구현중

--- src/Aaa.java
+++ src/Aaa.java
@@ -1,50 +1,5 @@
-import java.util.Scanner;
-
 public class Aaa {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("학생 수를 입력하세요: ");
-        int count = sc.nextInt();
-
-        int[] scores = new int[count];
-        int sum = 0;
-
-        // 점수 입력 + 유효성 검사
-        for (int i = 0; i < count; i++) {
-            System.out.printf("%d번 학생 점수: ", i + 1);
-            int score = sc.nextInt();
-            scores[i] = score;
-            sum += score;
-        }
-
-        double avg = (double) sum / count;
-        int avgDiv = (int) avg / 10;
-
-        char grade;
-
-        switch (avgDiv) {
-            case 10: // 100점
-            case 9:
-                grade = 'A';
-                break;
-            case 8:
-                grade = 'B';
-                break;
-            case 7:
-                grade = 'C';
-                break;
-            case 6:
-                grade = 'D';
-                break;
-            default:
-                grade = 'F';
-        }
-
-        System.out.println("\n총점: " + sum);
-        System.out.printf("평균: %.2f\n", avg);
-        System.out.println("등급: " + grade);
-
-        sc.close();
-    }
+	public static void main(String[] args) {
+	
+	}
 }
--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -9,93 +9,68 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
 		Scanner sc = new Scanner(System.in);
-		int id = 1; // while 문 안에 있으면 계속 1로 초기화 되기 때문에 2번이 안나온다. 그래서 while문 밖에 넣어놔야한다.
+		int id = 1; 
 		List<Article> articles = new ArrayList<Article>();
-		// 컬렉션프레임워크인 List를 상속받는 ArrayList를 사용한다.
-		// ArrayList는 자동으로 순서를 가진다.
-		// List는 Article 타입에 지정된 타입의 객체만을 저장한다.
-		// articles는<Article> class 타입이라고 선언만 함
 
 		while (true) {
 
 			System.out.printf("명령어) ");
-			String cmd = sc.nextLine().trim(); // trim() 추가
-//			System.out.println("cmd = " + cmd);
+			String cmd = sc.nextLine().trim(); 
+			
 			if (cmd.equals("exit")) {
 				break;
 			}
 			if (cmd.length() == 0) {
 				System.out.println("명령어를 입력해주세요.");
-				continue; // 다시 while문 처음으로 돌아가게함.
-			} // 하지만 공백이 있으면 존재하지않는 명령어입니다가 출력됨 -> 공백을 없애야한다. -> cmd에 trim()추가하기
+				continue; 
+			}
 
 			if (cmd.equals("article write")) {
 
-				System.out.printf("제목 ");
-				String title = sc.nextLine();
-				System.out.printf("내용 ");
-				String body = sc.nextLine();
+				System.out.printf("제목 : ");
+				String title = sc.nextLine().trim();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine().trim();
 
 				Article article = new Article(id, title, body);
-				// 최적화 1
-				// 생성자를 이용해서 초기화
-				// 인자를 통해 생성자를 호출
-
-//				Article article = new Article();				
-//				article.id = id;
-//				article.title = title;
-//				article.body = body;
-//				// article은 지역변수이므로 저장된것은 아니다
-//				// 일반 main에 선언했던 내용들을 atricle class로 데이터를 넣어줌
-
-				// 위에서 만든 articles에 넣어줘야한다.
 				articles.add(article);
-				// List구조인 ArrayList 객체인 articles에 저장(add)함
 
-				// 최적화 2
-//				articles.add(new Article(id,title,body)); //최적화1과 add까지 합쳐서 최적화함.
 
 				System.out.printf(id + "글이 생성되었습니다.\n");
 				id++;
 
-//			} else if (cmd.equals("test")){ // 글이 제대로 저장됐는지 테스트하는 if줄..
-//				for (int i = 0 ; i < articles.size() ; i++) {
-//					System.out.println(articles.get(i).id);
-//					System.out.println(articles.get(i).title);
-//					System.out.println(articles.get(i).body);
-//				}
-
 			} else if (cmd.equals("article List")) { // List 출력할때
 
 				if (articles.size() == 0) {
-					// size()는 arr가 얼마인지 계산해서 리턴하는 List의 주요 메소드
-					// 뜻 : articles(객체)의 사이즈가 0일때
 
 					System.out.println("게시글이 없습니다.");
 					continue; // while문 처음으로 돌아감.
 				}
 
-//if문시작		if (articles.size() != 0) { // articles가 0과 다를 때
-// if문이 두번일 필요가 있을까? 아니요, article List가 호출될때 기본적으로 적용되고
-// 0일경우만 if를 적용하도록 하면 됨.
 				System.out.println("번호 | 제목");
-//정방향 출력		for (int i = 0; i < articles.size(); i++) {
-				    
-				    for (int i = articles.size()- 1 ; i>=0 ; i-- ) { //역순으로 출력하기
+
+				for (int i = articles.size() - 1; i >= 0; i--) { // 역순으로 출력하기
 					Article article = articles.get(i); // get으로 가져와서
 					System.out.printf("%d    | %s \n", article.id, article.title); // 출력
-
-					// 또다른 방법, get과 출력을 같이
-					// System.out.printf("%d | %s \n", articles.get(i).id, articles.get(i).title);
 				}
-//if문끝나기    }
+			} else if(cmd.startsWith("article detail")){ //입력값이 ""로 시작하면
+				// startsWith() / 문자가 시작하는 것을 확인해서 비교 참or거짓
+				// article detail 시작하니?
+				System.out.println("1번 게시물이 존재하지 않습니다.");
+				
+				//split(",") ,로 문자열을 쪼개는 메소드
+				String[] cmdBits =cmd.split(" "); // cmd의 문자열을 공백을 기준으로 쪼갠다.(복수의 문자열로 나온다)
+				//쪼개지면 배열이 되기때문에 배열 String[]이 되어야한다.
+				System.out.println(cmdBits[0]);
+				System.out.println(cmdBits[1]);
+				System.out.println(cmdBits[2]);
+				
 			} else {
 				System.out.println("존재하지않는 명령어입니다.");
 			}
 		}
 		System.out.println("== 프로그램 종료 ==");
 		sc.close();
-
 	}
 }
 
