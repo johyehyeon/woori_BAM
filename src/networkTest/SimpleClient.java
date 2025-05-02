858ec1fd304c7d3ad7342338aeed8c201ad4f6e7
johyehyeon <johyehyeon92@gmail.com>
2025-05-02 10:57:09 +0900

    server, client 공부후 추가주석

--- src/networkTest/SimpleClient.java
+++ src/networkTest/SimpleClient.java
@@ -1,26 +1,36 @@
 package networkTest;
- 
- import java.io.BufferedReader;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
- 
- public class SimpleClient {
-     public static void main(String[] args) {
-         String serverIP = "127.0.0.1"; // 서버주소 (localhost = 내컴퓨터)
-         int port = 9999;  // 서버의 대기중인 포트번호
- 
-         try (Socket socket = new Socket(serverIP, port)) { // ip + 포트
-             // 서버에 메시지 전송
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-             out.println("안녕하세요");
- 
-             // 서버 응답 수신
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             String serverResponse = in.readLine();
-             System.out.println("서버 응답: " + serverResponse);
- 
+
+public class SimpleClient {
+
+	public static void main(String[] args) {
+		String serverIP = "127.0.0.1";  //접속할 서버주소( 127.0.0.1은 localhost = 내컴퓨터)
+		int port = 9999; // 서버와 통신할 포트주소, 서버와 동일해야한다.
+		// 서버의 ip 주소와 포트를 선언
+		
+		try (Socket socket = new Socket(serverIP,port)) {
+			// 소켓을 객체를 생성하여 서버와의 연결 시도.
+			
+			PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+			// 서버에 문자를 보내기 위한 출력 스트림
+			out.println("안녕하세요");
+			// 서버에 보내는 문자열
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			//getInputStream()-> 서버가 보낸 바이트 데이터를 가져옴
+			//InputStreamReader() 가져온 바이트 언어를 스트림으로 변환
+			// BufferedReader() = 문자스트림은 버퍼링해서 읽을수 있도록 도와주는 클래스 .readLine메서드를 사용
+			
+			String serverResponse = in.readLine();
+			//서버가 보내온 응답 읽기 readLine() 메서드로 가져오기
+			
+			System.out.println("서버응답 : " + serverResponse);
+			// 서버응답 출력
          } catch (IOException e) {
         	 // 방화벽이든 예상치못한 예외상황 발생시
              e.printStackTrace();
--- src/networkTest/SimpleServer.java
+++ src/networkTest/SimpleServer.java
@@ -1,38 +1,49 @@
-package networkTest;
- 
- import java.io.BufferedReader;
+  package networkTest;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
- 
- public class SimpleServer {
-     public static void main(String[] args) {
-         int port = 9999; // 서버 포트
-                                          // port 인자를 가지고있음
-                                          // throws IOException
-         try (ServerSocket serverSocket = new ServerSocket(port)) {
-             System.out.println("서버가 포트 " + port + "에서 대기 중입니다...");
-             //
- 
-             Socket clientSocket = serverSocket.accept();
-             System.out.println("클라이언트 연결됨: " + clientSocket.getInetAddress());
- 
-             // 데이터 수신
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-             String clientMessage = in.readLine();
-             // 한문장, 한라인단위로 읽는 메소드
-             System.out.println("클라이언트로부터 수신: " + clientMessage);
- 
-             // 데이터 송신 
-             // 외우기
-             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             out.println("접속 감사합니다.");
- 
-             clientSocket.close();
+import java.net.UnknownHostException;
+
+public class SimpleServer {
+
+	public static void main(String[] args) {
+		int port = 9999;
+		
+		try (ServerSocket serverSocket = new ServerSocket(port)) { 
+			System.out.println("서버가 포트 " +  port + "에서 서버가 준비되었습니다. 클라이언트를 기다리는중...");
+			
+			Socket clientSocket = serverSocket.accept(); 
+			// 클라이언트 접속을 대기하다가 접속요청이 오면 객체를 반환한다.
+			System.out.println("클라이언트 연결됨" + clientSocket.getInetAddress());
+			// 클라이언트 IP주소를 가져옴
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			//getInputStream()-> 클라이언트가 보낸 바이트 데이터를 가져옴
+			//InputStreamReader() 가져온 바이트 언어를 스트림으로 변환
+			// BufferedReader() = 문자스트림은 버퍼링해서 읽을수 있도록 도와주는 클래스 .readLine메서드를 사용
+			String clientMessage = in.readLine();
+			// readLine() 한줄단위로 읽는 메서드 clientMessage 변수에 메세지 담기
+			System.out.println("클라이언트로부터 수신 '" + clientMessage + "'");
+			// 클라이언트로부터 받은 메세지 출력
+			
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
+			// 클라이언트에게 문자를 보내기위한 출력 스트림.
+			out.println("접속감사합니다.");
+			// 클라이언트에 보내는 메세지 입력
+			
+			clientSocket.close();
+			System.out.println("서버종료");
              System.out.println("서버 종료");
-         } catch (IOException e) {
+             
+         } catch (UnknownHostException e) {
+        	 // 서버가 꺼져있거나, 서버가 거부를 했거나 등등..
+        	 e.printStackTrace();
+        	 
+         }catch (IOException e) {
         	 // 방화벽이든 예상치못한 예외상황 발생시
              e.printStackTrace();
              // 로그를 처리해라  
